bea27dcd5ad1cd2c79b06a0ce322d4874f4035a5
Alex Parker <alex.n.parker82@gmail.com>
2022-01-29 13:57:09 -0500

    site under construction

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
-    <CssBaseline />
-    <Header />
-    <div style={{ height: "700px" }}>
-
-    </div>
-    <Footer />
+      <CssBaseline />
+      <Header />
+      <div style={{ height: "700px", textAlign: "center" }}>
+        <h1 style={{ color: "white", marginTop: "200px", fontSize:"100px" }}>Site Under Construction!</h1>
+      </div>
+      <Footer />
     </>
   );
 }
